9c48c55cf4b56f9b01a29bc115c4956c8a23e3c6
1510460325@qq.com <1510460325@qq.com>
2019-07-27 21:13:25 +0800

    [add] 添加函数的认识，变量申明

--- service/UserService.go
+++ service/UserService.go
@@ -1,7 +1,7 @@
 package UserService
 
-var id = 13
+var Id = 13
 
 func Test(a int, b int) int {
-		return a - b + id
+	return a - b + Id
 }
--- test.go
+++ test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-   "fmt"
+	"./service"
+	"fmt"
 )
 
 func main() {
-   var a string = "abc"
-   fmt.Println("hello, world", a, a, nil)
+	var a string = "abc"
+	fmt.Println("hello, world", a)
+	fmt.Println(UserService.Test(1, 2))
+	fmt.Println(UserService.Id)
 }
