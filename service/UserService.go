ce6d22587c23188c67615a97b5b834fb8d2449da
1510460325@qq.com <1510460325@qq.com>
2019-07-29 23:11:13 +0800

    [add] 添加for循环和if语句

--- service/UserService.go
+++ service/UserService.go
@@ -2,6 +2,9 @@ package UserService
 
 var Id = 13
 
+/*
+  大写的函数或者全局变量才能被外包引用
+*/
 func Test(a int, b int) int {
 	return a - b + Id
 }
--- test.go
+++ test.go
@@ -6,8 +6,36 @@ import (
 )
 
 func main() {
-	var a string = "abc"
+	// := 赋值并申明
+	a := "abc"
+	b := UserService.Test(1, 2)
 	fmt.Println("hello, world", a)
 	fmt.Println(UserService.Test(1, 2))
 	fmt.Println(UserService.Id)
+	// if else 语句
+	if b > 2 {
+		fmt.Println(b)
+	} else {
+		fmt.Println(2)
+	}
+	// for循环
+	n := 5
+	i := 1
+	for i < n {
+		fmt.Println("当前运行到", i)
+		i++
+	}
+	// 函数闭包
+	fmt.Println(getI()())
+}
+
+/*
+  函数闭包：返回的是一个函数，再次调用则返回结果
+*/
+func getI() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
 }
