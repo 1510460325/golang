d106cc21b0896ee31551b8092e546fc84c52d452
1510460325@qq.com <1510460325@qq.com>
2019-09-10 19:49:21 +0800

    [add] 添加最长回文子串

---
+++ leetCode/longestPalindrome.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//var str string = "cyyoacmjwjubfkzrrbvquqkwhsxvmytmjvbborrtoiyotobzjmohpadfrvmxuagbdczsjuekjrmcwyaovpiogspbslcppxojgbfxhtsxmecgqjfuvahzpgprscjwwutwoiksegfreortttdotgxbfkisyakejihfjnrdngkwjxeituomuhmeiesctywhryqtjimwjadhhymydlsmcpycfdzrjhstxddvoqprrjufvihjcsoseltpyuaywgiocfodtylluuikkqkbrdxgjhrqiselmwnpdzdmpsvbfimnoulayqgdiavdgeiilayrafxlgxxtoqskmtixhbyjikfmsmxwribfzeffccczwdwukubopsoxliagenzwkbiveiajfirzvngverrbcwqmryvckvhpiioccmaqoxgmbwenyeyhzhliusupmrgmrcvwmdnniipvztmtklihobbekkgeopgwipihadswbqhzyxqsdgekazdtnamwzbitwfwezhhqznipalmomanbyezapgpxtjhudlcsfqondoiojkqadacnhcgwkhaxmttfebqelkjfigglxjfqegxpcawhpihrxydprdgavxjygfhgpcylpvsfcizkfbqzdnmxdgsjcekvrhesykldgptbeasktkasyuevtxrcrxmiylrlclocldmiwhuizhuaiophykxskufgjbmcmzpogpmyerzovzhqusxzrjcwgsdpcienkizutedcwrmowwolekockvyukyvmeidhjvbkoortjbemevrsquwnjoaikhbkycvvcscyamffbjyvkqkyeavtlkxyrrnsmqohyyqxzgtjdavgwpsgpjhqzttukynonbnnkuqfxgaatpilrrxhcqhfyyextrvqzktcrtrsbimuokxqtsbfkrgoiznhiysfhzspkpvrhtewthpbafmzgchqpgfsuiddjkhnwchpleibavgmuivfiorpteflholmnxdwewj"
+	fmt.Println(longestPalindrome("ac"))
+}
+
+func longestPalindrome(s string) string {
+	if s == "" {
+		return ""
+	}
+	if len(s) == 1 {
+		return s
+	}
+	dp := [1000][1000]int{{}}
+	start, maxLen := 0, 1
+	for i := len(s) - 2; i >= 0; i-- {
+		dp[i][i] = 1
+		for j := i + 1; j < len(s); j++ {
+			if s[i] == s[j] {
+				if j-i == 1 {
+					dp[i][j] = 2
+				} else {
+					if dp[i+1][j-1] > 0 {
+						dp[i][j] = dp[i+1][j-1] + 2
+					} else {
+						dp[i][j] = 0
+					}
+				}
+			} else {
+				dp[i][j] = 0
+			}
+			if dp[i][j] > maxLen {
+				maxLen = dp[i][j]
+				start = i
+			}
+		}
+	}
+	return string([]byte(s)[start : start+maxLen])
+}
