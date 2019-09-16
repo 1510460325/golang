d8d9bbce4322ba3281fc288e2e0c6bb3c21baabd
1510460325@qq.com <1510460325@qq.com>
2019-09-16 20:06:33 +0800

    [add] 添加Golang方法绑定中的*区别

--- structs/extends/ExtendsDemo.go
+++ structs/extends/ExtendsDemo.go
@@ -7,22 +7,26 @@ type Person struct {
 	id   int
 }
 
-func (person Person) sayHello() {
+func (person *Person) sayHello() {
 	fmt.Printf("hello, i'm %s\n", person.name)
+	person.name = "changedName"
+}
+
+func (person *Person) introduce() {
+	fmt.Printf("my name is %s\n", person.name)
 }
 
 type Female struct {
 	Person
 }
 
-func (female Female) sayHello() {
+func (female *Female) sayHello() {
 	female.Person.sayHello()
-	fmt.Printf("repeat it")
+	fmt.Println("im done")
 }
 
 func main() {
-	person := Person{"abc", 1}
-	person.sayHello()
 	female := Female{Person{"name", 12}}
 	female.sayHello()
+	female.introduce()
 }
