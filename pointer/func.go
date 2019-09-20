71173e832442f7972b7cb4c582f28cfd0c10b208
1510460325@qq.com <1510460325@qq.com>
2019-09-20 17:02:47 +0800

    [add] 函数签名 函数类型相当于一个函数指针

--- pointer/func.go
+++ pointer/func.go
@@ -1,8 +1,21 @@
 package main
 
+import "fmt"
+
 /*
  函数类型
 */
-func main() {
 
+type Op func(int, int) int
+
+func do(op Op, a int, b int) {
+	fmt.Println(op(a, b))
+}
+
+func add(a, b int) int {
+	return a + b
+}
+func main() {
+	fmt.Printf("%T\n", add)
+	do(add, 10, 10)
 }
