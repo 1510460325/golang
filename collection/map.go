08c04ea6981bbebe437140854f390862a71d8c5b
1510460325@qq.com <1510460325@qq.com>
2019-10-21 19:28:49 +0800

    [add] map的查询操作

--- collection/map.go
+++ collection/map.go
@@ -7,6 +7,8 @@ func main() {
 	m[1] = 1
 	m[2] = 1
 	m[3] = 1
+	val, ok := m[2]
+	fmt.Printf("是否存在：%v, 值为:%d\n", ok, val)
 	for key, val := range m {
 		fmt.Printf("key is %d => %d\n", key, val)
 	}
